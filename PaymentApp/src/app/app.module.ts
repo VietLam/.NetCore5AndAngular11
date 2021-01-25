47fdd0ed0621f1ac4424dd19b5543b682e6bb5e1
travixlam <travixlam@gmail.com>
2021-01-25 17:50:10 +0700

    Commit source final

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,8 @@
 import { PaymentDetailService } from './../../shared/payment-detail.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
 
 @Component({
   selector: 'app-payment-detail-form',
@@ -9,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentDetailFormComponent implements OnInit {
 
-  constructor(public service: PaymentDetailService) { }
+  constructor(
+    public service: PaymentDetailService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if (this.service.formData.paymentDetailId == 0) {
+      this.insertRecord(form);
+    } else {
+      this.updateRecord(form);
+    }
+  }
+
+  insertRecord(form: NgForm) {
+    this.service.formData.paymentDetailId = 0;
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        this.toastr.success('Insert success', 'Payment Detail Regist');
+      },
+      err => {
+        this.toastr.error('Insert error', 'Payment Detail Regist');
+      }
+    );
+  }
+
+  updateRecord(form: NgForm) {
+    this.service.putPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        this.toastr.success('Update success', 'Payment Detail Regist');
+      },
+      err => {
+        this.toastr.error('Updates error', 'Payment Detail Regist');
+      }
+    );
+  }
+
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
   }
 
 }
